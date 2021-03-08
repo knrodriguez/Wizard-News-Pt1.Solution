4d99726c3d789755380eda1e5598def0b51e7499
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-03-08 11:40:25 -0500

    Listing Posts

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require('morgan');
 const app = express();
+const postBank = require('./postBank');
 
 app.use(morgan('dev'));
 
@@ -11,7 +12,23 @@ const volleyball = require('volleyball');
 app.use(volleyball)
 */
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html = `<!DOCTYPE html>
+    <html>
+      <head>
+        <title>Wizard News</title>
+      </head>
+      <body>
+        <ul>
+<!-- arrow function shorthand, doesn't need return keyword if function body is one line of code -->
+          ${posts.map(post => `<li>${post.title} by ${post.name}</li>`)} 
+        </ul>
+      </body>
+    </html>`;
+  
+  res.send(html);
+});
 
 const PORT = 1337;
 
