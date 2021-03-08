a669de3721270b15f6cb8182b15a11138198a9b3
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-03-08 11:54:46 -0500

    Add a Single-Post Route

--- app.js
+++ app.js
@@ -45,6 +45,32 @@ app.get("/", (req, res) => {
   res.send(html);
 });
 
+app.get('/posts/:id', (req,res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  const html = `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+          <div class='news-item'>
+            <p>
+             ${post.title}
+              <small>(by ${post.name})</small>
+            </p>
+            <p>${post.content}</p>
+          </div>
+      </div>
+    </body>
+  </html>`;
+
+  res.send(html);
+})
+
 const PORT = 1337;
 
 app.listen(PORT, () => {
