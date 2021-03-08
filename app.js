b6211f213a59d4da009e5838ad46e2013c5fce4f
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-03-08 12:13:13 -0500

    Date Formatting

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require('morgan');
 const app = express();
 const postBank = require('./postBank');
+const timeAgo = require('node-time-ago');
 
 app.use(morgan('dev'));
 
@@ -34,7 +35,7 @@ app.get("/", (req, res) => {
               <small>(by ${post.name})</small>
             </p>
             <small class="news-info">
-              ${post.upvotes} upvotes | ${post.date}
+              ${post.upvotes} upvotes | ${timeAgo(post.date)}
             </small>
           </div>`
         ).join('')}
